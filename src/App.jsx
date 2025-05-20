ac95ad918417aed500bda9d240c261e36b753715
jatinsiwach2511 <jatin@memorres.com>
2025-05-20 12:54:50 +0530

    added multiple triggers

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+// App.js
+import React from 'react';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import MyDocument from './Pdf';
 
-const App = () =>{
-  return <div>
-    knkvnkrvnrkkgfuifghrifhekfefkjfk
+const App = () => (
+  <div>
+    <h2>PDF with Image</h2>
+    <PDFDownloadLink document={<MyDocument />} fileName="logo-pdf.pdf">
+      {({ loading }) => (loading ? 'Loading PDF...' : 'Download PDF')}
+    </PDFDownloadLink>
   </div>
-}
-export default App
+);
+
+export default App;
